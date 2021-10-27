2083a7c48c6e03608b61b63bca747799891152fc
Nicholas C <nicholas.chu98@gmail.com>
2021-10-26 22:44:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+/*
+ Authors:
+ Your name and student #: Nicholas Chu A01193251
+*/
+const express = require("express");
+const fs = require("fs")
+
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+var movieNames = []
+var movieNamesLower = []
+const name = "Nick"
+
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/myForm", (req, res) => res.render("pages/myForm"));
+
+app.post("/myForm", (req, res) => {
+  // Add your implementation here
+  let formData = req.body;
+  const movieData = formData["MovieList"].split(', ')
+  movieNames = movieData
+  movieNamesLower = movieData.map(name => name.toLowerCase());
+  res.render("pages/index", {Name: name, movieData: movieData, movieNamesLower: movieNamesLower});
+});
+
+app.get("/myListQueryString", (req, res) => {
+  // Add your implementation here
+  let movie1 = req.query.movie1
+  let movie2 = req.query.movie2 
+  const moviequery = [movie1, movie2]
+  res.render("pages/index", {Name: name, movieData: movieNames, movieNamesLower: moviequery})
+});
+
+app.get("/search/:movieName", (req, res) => {
+  // Add your implementation here
+  const empty_error = "Movie Could Not Be Found"
+  let search = req.params.movieName.toLowerCase
+  fs.readFile("./movieDescriptions.txt", (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      let content = data.toString()
+      const movies = content.split('\n')
+      for (i = 0; i < movies.length; i++) {
+        const content = movies[i].split(':')
+        if (content[0].toLowerCase === search) {
+          res.render("pages/searchResult", {movieTitle: content[0], movieDesc: content[1]})
+        } else {
+          res.render("pages/searchResult", {movieTitle: "", movieDesc: empty_error})
+        }
+      }
+    }
+  })
+  
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 🚀");
+});
